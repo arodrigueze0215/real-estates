51040d76cc5aeb7d7614e8da9db5a3e73f1d1b9c
arodrigueze0215 <andres.rodriguez0215@gmail.com>
2021-09-22 09:18:05 -0500

    Adding Navbar

---
+++ components/Navbar/MenuContainer.jsx
@@ -0,0 +1,13 @@
+
+const MenuContainer = () => {
+    return (
+        <div className="flex flex-row justify-around invisible sm:visible">
+            <div className="px-4 flex items-center">Cundinamarca</div>
+            <div className="px-4 flex items-center">Antioquia</div>
+            <div className="px-4 flex items-center">Recursos</div>
+            <div className="px-4 flex items-center">Mi Perfil</div>
+        </div>
+    )
+    
+}
+export default MenuContainer
